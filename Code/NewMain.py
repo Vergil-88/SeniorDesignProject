ca6a7e4cdb7ea3dfdb7e33544522131c57aeb84d
Vergil-88 <91845426+Vergil-88@users.noreply.github.com>
2024-02-29 01:49:08 +0300

    Cleaned code
    moayad and Meshari

---
+++ Code/NewMain.py
@@ -0,0 +1,227 @@
+from time import sleep
+import threading
+<<<<<<< Updated upstream
+import os
+import time
+=======
+>>>>>>> Stashed changes
+import TCPLink
+import math
+import Comm
+import uwb
+
+
+<<<<<<< Updated upstream
+def calc_dis_ang(x1, y1, x2, y2):
+    # Calculate distance
+    distance = math.sqrt((x2 - x1) ** 2 + (y2 - y1) ** 2)
+    
+    # Calculate angle in radians
+    angle_radians = math.atan2(y2 - y1, x2 - x1)
+    
+    # Convert angle to degrees
+    angle_degrees = math.degrees(angle_radians)
+    
+    return distance, angle_degrees
+    
+# my_thread = threading.Thread(target=uwb.main)
+# # Start the thread
+# my_thread.start()   
+
+my_GY_thread = threading.Thread(target=Comm.main)
+# Start the thread
+my_GY_thread.start() 
+
+# TCPLink.send(S,0,0)
+# TCPLink.receive(S,False)
+
+    
+
+# cntR_prev=TCPLink.cntR_int
+# cntL_prev=TCPLink.cntL_int
+
+# if cntR_prev > 4500:
+#     cntR_prev-=9000
+
+# if cntL_prev > 4500:
+#     cntL_prev-=9000
+
+# dTheta = 0
+=======
+S=TCPLink.TCP_init()
+TCPLink.send(S,0,0)
+TCPLink.receive(S,False)
+
+## ENCODER INIT (IN A FUNCTION)
+cntR_prev=TCPLink.cntR_int
+cntL_prev=TCPLink.cntL_int
+if cntR_prev > 4500:
+    cntR_prev-=9000
+
+if cntL_prev > 4500:
+    cntL_prev-=9000
+###############
+>>>>>>> Stashed changes
+
+
+my_GY_thread = threading.Thread(target=Comm.main)
+my_GY_thread.start() 
+
+<<<<<<< Updated upstream
+# print("intial cntR_prev",cntR_prev)
+# print("intial cntL_prev",cntL_prev)
+
+# print("intial cntR",TCPLink.cntR_int)
+# print("intial cntL",TCPLink.cntL_int)
+=======
+Encdr_angle, Encdr_Distance, Desired_Angle, gyro_Angle, cntR_int, cntL_int, R_start, L_start = 0
+>>>>>>> Stashed changes
+
+sleep(20)
+
+X= uwb.x
+Y= uwb.y
+
+Encdr_Distance = (Encdr_Distance*9)/400
+
+
+
+def encoder_calcs(cntR,cntL):
+    
+    if (cntR-cntR_prev)>4500 or cntR>8000:
+         cntR-=9000
+         
+
+    if (cntL-cntL_prev)>4500 or cntL>8000:
+         cntL-=9000
+         
+
+    dR=cntR-cntR_prev
+    dL=cntL-cntL_prev
+    k=-0.5
+    
+    Encdr_angle = k * (dR-dL)
+
+    return Encdr_angle
+
+
+
+def calc_dis_ang(x1, y1, x2, y2): # Distance and angle USING THE BEACONS
+    # Calculate distance
+    distance = math.sqrt((x2 - x1) ** 2 + (y2 - y1) ** 2)
+    
+    # Calculate angle in radians
+    angle_radians = math.atan2(y2 - y1, x2 - x1)
+    
+    # Convert angle to degrees
+    angle_degrees = math.degrees(angle_radians)
+    
+    return distance, angle_degrees
+
+
+
+
+<<<<<<< Updated upstream
+print("Distance",Distance)
+print("Angle",Angle)
+print(X,",",Y)
+
+execute_once = True
+
+S=TCPLink.TCP_init()
+TCPLink.send(S,0,0)
+
+start_time = time.time()
+
+=======
+>>>>>>> Stashed changes
+while True:
+
+    TCPLink.receive(S,True)     
+
+    gyro_Angle=Comm.angle_values
+    cntR_int=TCPLink.cntR_int
+    cntL_int=TCPLink.cntL_int
+
+<<<<<<< Updated upstream
+    if execute_once:
+        # The line of code you want to execute only once
+        cntR_prev=cntR_int
+        cntL_prev=cntL_int
+
+        if cntR_prev > 4500:
+            cntR_prev-=9000
+
+        if cntL_prev > 4500:
+            cntL_prev-=9000
+
+        dTheta = 0
+
+
+
+        print("intial cntR_prev",cntR_prev)
+        print("intial cntL_prev",cntL_prev)
+
+        print("intial cntR",TCPLink.cntR_int)
+        print("intial cntL",TCPLink.cntL_int)
+
+        
+        # Set the flag to False so this block doesn't execute again
+        execute_once = False
+    
+
+    # print("current cntR",TCPLink.cntR_int)
+    # print("current cntL",TCPLink.cntL_int)
+
+    encoder_calcs(cntR_int,cntL_int)     
+    
+    
+    
+    meters=1
+    margin=0.5
+    # print(angle)
+    print("gyro_Angle",gyro_Angle)
+
+    # R_start=cntR_int
+    # L_start=cntL_int   
+    Angle=90
+
+    timeAngle = ((time.time() - start_time)*360)/50 #calcolate current angle using time
+
+    print("timeAngle",timeAngle)
+
+    avrgTheta = (dTheta + gyro_Angle+timeAngle)/3
+    print("avrgTheta",avrgTheta)
+     
+    if  abs(avrgTheta)<=abs(Angle)   :
+
+        
+        if Angle <0 and Angle != 0:
+=======
+    Encdr_angle = encoder_calcs(cntR_int,cntL_int)     
+    
+
+    Desired_Angle=90
+    if  abs(Encdr_angle)<=abs(Desired_Angle):
+        if Desired_Angle < 0 and Desired_Angle != 0:
+>>>>>>> Stashed changes
+            
+            TCPLink.send(S,0,-10)
+        elif Desired_Angle != 0 :
+            TCPLink.send(S,0,10)
+            # break
+        else:
+            TCPLink.send(S,0,0)
+
+        R_start=cntR_int
+        L_start=cntL_int  
+
+    
+    
+    # elif ((cntR_int-R_start)<=166*Distance and (cntL_int-L_start)<=166*Distance) :
+        
+    #     TCPLink.send(S,10,0)
+    # else:
+        
+    #     TCPLink.send(S,0,0)
+    #     break
--- Code/OldMainUNUSED.py
+++ Code/OldMainUNUSED.py
@@ -1,12 +1,14 @@
 from time import sleep
 import threading
 import os
-import time
 import TCPLink
 # import uwb
 import math
 import Comm
 
+
+
+S=TCPLink.TCP_init()
 
 
 def calc_dis_ang(x1, y1, x2, y2):
@@ -29,29 +31,23 @@ my_GY_thread = threading.Thread(target=Comm.main)
 # Start the thread
 my_GY_thread.start() 
 
-# TCPLink.send(S,0,0)
-# TCPLink.receive(S,False)
-
+TCPLink.send(S,0,0)
+TCPLink.receive(S,False)
     
 
-# cntR_prev=TCPLink.cntR_int
-# cntL_prev=TCPLink.cntL_int
+cntR_prev=TCPLink.cntR_int
+cntL_prev=TCPLink.cntL_int
 
-# if cntR_prev > 4500:
-#     cntR_prev-=9000
+if cntR_prev > 4500:
+    cntR_prev-=9000
 
-# if cntL_prev > 4500:
-#     cntL_prev-=9000
-
-# dTheta = 0
+if cntL_prev > 4500:
+    cntL_prev-=9000
 
 
 
-# print("intial cntR_prev",cntR_prev)
-# print("intial cntL_prev",cntL_prev)
-
-# print("intial cntR",TCPLink.cntR_int)
-# print("intial cntL",TCPLink.cntL_int)
+print("intial cntR_prev",cntR_prev)
+print("intial cntL_prev",cntL_prev)
 
 
 
@@ -60,11 +56,11 @@ my_GY_thread.start()
 def encoder_calcs(cntR,cntL):
     global cntR_prev,cntL_prev,dTheta
     
-    if (cntR-cntR_prev)>4500 or cntR>8000:
+    if (cntR-cntR_prev)>2000 or cntR>8000:
          cntR-=9000
          
 
-    if (cntL-cntL_prev)>4500 or cntL>8000:
+    if (cntL-cntL_prev)>2000 or cntL>8000:
          cntL-=9000
          
 
@@ -116,14 +112,8 @@ Distance=(Distance*9)/400
 print("Distance",Distance)
 print("Angle",Angle)
 print(X,",",Y)
-
-execute_once = True
-
-S=TCPLink.TCP_init()
+    
 TCPLink.send(S,0,0)
-
-start_time = time.time()
-
 while True:
     # print("Distance",Distance)
     # print("Angle",Angle)
@@ -134,35 +124,6 @@ while True:
     cntR_int=TCPLink.cntR_int
     cntL_int=TCPLink.cntL_int
 
-    if execute_once:
-        # The line of code you want to execute only once
-        cntR_prev=cntR_int
-        cntL_prev=cntL_int
-
-        if cntR_prev > 4500:
-            cntR_prev-=9000
-
-        if cntL_prev > 4500:
-            cntL_prev-=9000
-
-        dTheta = 0
-
-
-
-        print("intial cntR_prev",cntR_prev)
-        print("intial cntL_prev",cntL_prev)
-
-        print("intial cntR",TCPLink.cntR_int)
-        print("intial cntL",TCPLink.cntL_int)
-
-        
-        # Set the flag to False so this block doesn't execute again
-        execute_once = False
-    
-
-    # print("current cntR",TCPLink.cntR_int)
-    # print("current cntL",TCPLink.cntL_int)
-
     encoder_calcs(cntR_int,cntL_int)     
     
     
@@ -170,20 +131,12 @@ while True:
     meters=1
     margin=0.5
     # print(angle)
-    print("gyro_Angle",gyro_Angle)
+    # print(gyro_Angle)
 
     # R_start=cntR_int
     # L_start=cntL_int   
     Angle=90
-
-    timeAngle = ((time.time() - start_time)*360)/50 #calcolate current angle using time
-
-    print("timeAngle",timeAngle)
-
-    avrgTheta = (dTheta + gyro_Angle+timeAngle)/3
-    print("avrgTheta",avrgTheta)
-     
-    if  abs(avrgTheta)<=abs(Angle)   :
+    if  abs(dTheta)<=abs(Angle)   :
 
         
         if Angle <0 and Angle != 0:
@@ -191,7 +144,6 @@ while True:
             TCPLink.send(S,0,-10)
         elif Angle != 0 :
             TCPLink.send(S,0,10)
-            # break
         else:
             TCPLink.send(S,0,0)
 
